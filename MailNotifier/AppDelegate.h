a3b2c4486d335a2bc3a7717f3d2c08ab4ae45abe
Albert Zhang <edistein.zhang@gmail.com>
2015-01-15 18:04:04 +0800

    updated the menu items.

--- MailNotifier/AppDelegate.h
+++ MailNotifier/AppDelegate.h
@@ -8,11 +8,30 @@
 
 #import <Cocoa/Cocoa.h>
 
+#define kMailIndexToInsertInMenu 4
+
+#define kMaxMailsToFetch 1000
+
+typedef enum {
+    CheckingStatusLookingUp,
+    CheckingStatusError,
+    CheckingStatusWait4Retry,
+    CheckingStatusIdle
+}CheckingStatus;
+
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 {
-    NSMenu *statusMenu;
-    NSStatusItem *statusItem;
-//    NSMenuItem *
+    NSMenu *statusBarMenu;
+    NSStatusItem *statusBarItem;
+    NSMenuItem *appStatusMenuItem; // all mails are read, or unread count
+    NSMenuItem *checkNowMenuItem;
+    NSMenuItem *preferencesMenuItem;
+    NSMenuItem *aboutMenuItem;
+    NSMenuItem *exitMenuItem;
+
+    NSMutableArray *mailMenuItems;
+
+    BOOL unreadCount;
 }
 
 @end
--- MailNotifier/AppDelegate.m
+++ MailNotifier/AppDelegate.m
@@ -19,28 +19,110 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 
-    statusMenu = [[NSMenu alloc] init];
-
-    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    [statusItem setMenu:statusMenu];
-    [statusItem setTitle:@"Mail"];
-//    [statusItem setImage:[NSImage imageNamed:@"StatusBarIcon"]];
-    [statusItem setHighlightMode:YES];
-
-    [statusMenu addItem:[NSMenuItem separatorItem]];
-    [statusMenu addItem:[NSMenuItem separatorItem]];
-
-    [statusMenu addItemWithTitle:@"Exit" action:@selector(onExit:) keyEquivalent:@"X"];
+    [self createMenu];
+}
 
 
-    [self asdf];
+- (void)createMenu{
+    statusBarMenu = [[NSMenu alloc] init];
+
+    statusBarItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    statusBarItem.menu = statusBarMenu;
+    statusBarItem.title = @"Mail";
+    //    [statusItem setImage:[NSImage imageNamed:@"StatusBarIcon"]];
+    statusBarItem.highlightMode = YES;
+
+    appStatusMenuItem = [[NSMenuItem alloc] init];
+    appStatusMenuItem.title = @"";
+    appStatusMenuItem.enabled = NO;
+    [statusBarMenu addItem:appStatusMenuItem];
+
+    [statusBarMenu addItem:[NSMenuItem separatorItem]];
+
+    checkNowMenuItem = [[NSMenuItem alloc] init];
+    checkNowMenuItem.title = @"Check Now";
+    checkNowMenuItem.action = @selector(onCheckNow:);
+    checkNowMenuItem.keyEquivalent = @"C";
+    checkNowMenuItem.keyEquivalentModifierMask = 0;
+    [statusBarMenu addItem:checkNowMenuItem];
+
+    [statusBarMenu addItem:[NSMenuItem separatorItem]];
+    // place holder for the mails
+    [statusBarMenu addItem:[NSMenuItem separatorItem]];
+
+    preferencesMenuItem = [[NSMenuItem alloc] init];
+    preferencesMenuItem.title = @"Perferences...";
+    preferencesMenuItem.action = @selector(onPerferences:);
+    preferencesMenuItem.keyEquivalent = @"P";
+    preferencesMenuItem.keyEquivalentModifierMask = 0;
+    [statusBarMenu addItem:preferencesMenuItem];
+
+    aboutMenuItem = [[NSMenuItem alloc] init];
+    aboutMenuItem.title = @"About...";
+    aboutMenuItem.action = @selector(onAbout:);
+    aboutMenuItem.keyEquivalent = @"A";
+    aboutMenuItem.keyEquivalentModifierMask = 0;
+    [statusBarMenu addItem:aboutMenuItem];
+
+    [statusBarMenu addItem:[NSMenuItem separatorItem]];
+
+    exitMenuItem = [[NSMenuItem alloc] init];
+    exitMenuItem.title = @"Exit";
+    exitMenuItem.action = @selector(onExit:);
+    exitMenuItem.keyEquivalent = @"Q";
+    exitMenuItem.keyEquivalentModifierMask = 0;
+    [statusBarMenu addItem:exitMenuItem];
+    
+    
+    [self checkNow];
+}
+
+- (void)onCheckNow:(id)sender{
+    [self checkNow];
+}
+
+
+- (void)onPerferences:(id)sender{
+
+}
+
+- (void)onAbout:(id)sender{
+
 }
 
 - (void)onExit:(id)sender{
     [[NSApplication sharedApplication] terminate:nil];
 }
 
-- (void)asdf{
+- (void)setCheckingStatus:(CheckingStatus)st{
+    NSString *str = nil;
+    switch (st) {
+        case CheckingStatusError:
+            str = @"Error occured";
+            break;
+        case CheckingStatusLookingUp:
+            str = @"Looking Up ....";
+            break;
+        case CheckingStatusWait4Retry:
+            str = @"Will try again soon";
+            break;
+        case CheckingStatusIdle:
+            if(unreadCount > 0){
+                str = [NSString stringWithFormat:@"You have %d unread mails", unreadCount];
+            }else{
+                str = @"Ahh, all mails are read";
+            }
+            break;
+        default:
+            break;
+    }
+    appStatusMenuItem.title = str;
+}
+
+
+- (void)checkNow{
+    [self setCheckingStatus:CheckingStatusLookingUp];
+
     MCOIMAPSession *session = [[MCOIMAPSession alloc] init];
     [session setHostname:@"imap.qiye.163.com"];
     [session setPort:993];
@@ -50,42 +132,54 @@
 
     MCOIMAPMessagesRequestKind requestKind = MCOIMAPMessagesRequestKindHeaders | MCOIMAPMessagesRequestKindFlags;
     NSString *folder = @"INBOX";
-    MCOIndexSet *uids = [MCOIndexSet indexSetWithRange:MCORangeMake(0, 1000)];
+    MCOIndexSet *uids = [MCOIndexSet indexSetWithRange:MCORangeMake(0, kMaxMailsToFetch)];
 
-    //    MCOIMAPFetchMessagesOperation *fetchOperation = [session fetchMessagesByUIDOperationWithFolder:folder requestKind:requestKind uids:uids];
     MCOIMAPFetchMessagesOperation *fetchOperation = [session fetchMessagesOperationWithFolder:folder requestKind:requestKind uids:uids];
 
     [fetchOperation start:^(NSError * error, NSArray * fetchedMessages, MCOIndexSet * vanishedMessages) {
-        //We've finished downloading the messages!
-
-        //Let's check if there was an error:
         if(error) {
             NSLog(@"Error downloading message headers:%@", error);
-        }
+            [self setCheckingStatus:CheckingStatusError];
 
-        //And, let's print out the messages...
-//        NSLog(@"The post man delivereth:%@, \n%@", fetchedMessages, vanishedMessages);
+        }else{
+            unreadCount = 0;
 
-        for(MCOIMAPMessage *msg in fetchedMessages){
-            NSString *subject = msg.header.subject;
-            if(! subject){
-                subject = @"<Empty>";
+            if(mailMenuItems){
+                for(NSMenuItem *itm in mailMenuItems){
+                    [statusBarMenu removeItem:itm];
+                }
             }
-            BOOL isRead = msg.flags & MCOMessageFlagSeen;
-            NSMenuItem *itm = [[NSMenuItem alloc] init];
-            if(isRead){
-                itm.title = subject;
-            }else{
-                NSFont *boldf = [[NSFontManager sharedFontManager] fontWithFamily:statusMenu.font.familyName
-                                                          traits:NSBoldFontMask
-                                                          weight:0
-                                                            size:statusMenu.font.fontDescriptor.pointSize];
 
-                NSDictionary *attrs = @{NSFontAttributeName: boldf};
-                itm.attributedTitle = [[NSAttributedString alloc] initWithString:subject attributes:attrs];
+            mailMenuItems = [NSMutableArray array];
+
+            for(int i=0; i<fetchedMessages.count; i++){
+                MCOIMAPMessage *msg = fetchedMessages[i];
+
+                NSString *subject = msg.header.subject;
+                if(! subject){
+                    subject = @"<No Subject>";
+                }
+                BOOL isRead = msg.flags & MCOMessageFlagSeen;
+                NSMenuItem *itm = [[NSMenuItem alloc] init];
+                if(isRead){
+                    itm.title = subject;
+                }else{
+                    unreadCount ++;
+                    NSFont *boldf = [[NSFontManager sharedFontManager] fontWithFamily:statusBarMenu.font.familyName
+                                                              traits:NSBoldFontMask
+                                                              weight:0
+                                                                size:statusBarMenu.font.fontDescriptor.pointSize];
+
+                    NSDictionary *attrs = @{NSFontAttributeName: boldf};
+                    itm.attributedTitle = [[NSAttributedString alloc] initWithString:subject attributes:attrs];
+                }
+                [itm setAction:@selector(onSelectMail:)];
+                [statusBarMenu insertItem:itm atIndex:kMailIndexToInsertInMenu];
+
+                [mailMenuItems addObject:itm];
             }
-            [itm setAction:@selector(onSelectMail:)];
-            [statusMenu insertItem:itm atIndex:1];
+
+            [self setCheckingStatus:CheckingStatusIdle];
         }
     }];
 }
